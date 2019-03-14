d15bdc369aabad21e6c28e1b98e282c3df392c69
Aleksandr Bukreev <abukreev@gmail.net>
2019-03-14 21:24:32 +0300

    Focus switch works

--- fitrix.py
+++ fitrix.py
@@ -22,6 +22,9 @@ root = None
 citrixWindow = None
 NET_WM_NAME = None
 WM_NAME = None
+NET_ACTIVE_WINDOW = None
+forward_keys = False
+
 
 def childrenOfWindow(window):
     try:
@@ -32,21 +35,27 @@ def childrenOfWindow(window):
     except Xlib.error.BadWindow:
         raise StopIteration
 
-def windowIsOurs(window):
+def parentsOfWindiow(window):
+    try:
+        yield window.query_tree().parent
+    except Xlib.error.BadWindow:
+        raise StopIteration
+
+def isOurWindow(window):
     try:
         return SUBSTRING in getWindowTitle(window)
     except UnicodeDecodeError:
         return False
 
 def findOurWindowUnderThis(parent):
-    if windowIsOurs(parent):
+    if isOurWindow(parent):
         return parent
     for window in childrenOfWindow(parent):
-        try:
-            if windowIsOurs(window):
-                return window
-        except error.BadWindow:
-            continue
+        if isOurWindow(window):
+            return window
+    for window in parentsOfWindiow(parent):
+        if isOurWindow(window):
+            return window
     return None
 
 def initWindow(window):
@@ -58,34 +67,47 @@ def initWindow(window):
 def printWinInfo(window):
     title = getWindowTitle(window)
     pid = getPidByWindow(window)
-    print 'pid = {}; window id = {}; title = {}'.format(hex(window.id), pid, title)
+    winid = hex(window.id) if window else None
+    print 'pid = {}; window id = {}; title = {}'.format(winid, pid, title)
 
 def getWindowTitle(window):
-    return str(window.get_wm_name())
+    try:
+        return str(window.get_wm_name())
+    except AttributeError:
+        return None
 
 def getPidByWindow(window):
-    type_atom = display.intern_atom('_NET_WM_PID')
-    try:
-        type = window.get_full_property(type_atom, X.AnyPropertyType)
-        try:
+    if window:
+      type_atom = display.intern_atom('_NET_WM_PID')
+      try:
+          type = window.get_full_property(type_atom, X.AnyPropertyType)
+          if type:
             pid = type.value[0]
             return pid
-        except AttributeError:
-            pass
-    except error.BadWindow:
-        pass
+      except error.BadWindow:
+          pass
     return None
 
 def checkWindow(window):
-    if windowIsOurs(window):
+    global citrixWindow
+    ourWindow = None
+    if isOurWindow(window):
         ourWindow = window
     else: 
         ourWindow = findOurWindowUnderThis(window)
-    printWinInfo(ourWindow)
-    global citrixWindow
-    citrixWindow = ourWindow
-    initWindow(ourWindow)
-    #grabKeys(ourWindow)
+
+    if ourWindow:
+        citrixWindow = ourWindow
+        initWindow(citrixWindow)
+        print ('Found our window')
+        printWinInfo(citrixWindow)
+        grabKeys(citrixWindow)
+    elif citrixWindow:
+        citrixWindow = None
+        print ('Lost our window')
+        ungrabKeys()
+
+    printWinInfo(citrixWindow)
 
 def grabKey(window, key):
     window.grab_key(
@@ -102,7 +124,8 @@ def grabKeys(window):
     window.grab_keyboard(True, X.GrabModeAsync, X.GrabModeAsync, X.CurrentTime)
 
 def ungrabKeys():
-    display.ungrab_keyboard(1, X.CurrentTime)
+    global display
+    display.ungrab_keyboard(0, X.CurrentTime)
 
 def pressKey(window, keycode):
     sendKey(window, keycode, Xlib.protocol.event.KeyPress)
@@ -133,45 +156,45 @@ def init():
     NET_WM_NAME = display.intern_atom('_NET_WM_NAME')  # UTF-8
     global WM_NAME
     WM_NAME = display.intern_atom('WM_NAME')         
-    root.change_attributes(event_mask = Xlib.X.FocusChangeMask)
+    global NET_ACTIVE_WINDOW
+    NET_ACTIVE_WINDOW = display.intern_atom('_NET_ACTIVE_WINDOW')
+    root.change_attributes(event_mask = Xlib.X.PropertyChangeMask)
 
 def processEvents():
     while True:
         event = root.display.next_event()
-        print event
+#        print event
         processKeyEvent(event)
-        processCreateEvent(event)
-        processFocusEvent(event)
+#        processCreateEvent(event)
+        processPropertyEvent(event)
 
 def processKeyEvent(event):
-    if event.type == X.KeyPress:
-        keycode = event.detail
-        if keycode in KEYS:
+    if citrixWindow:
+        print ("processKeyEvent")
+        if event.type == X.KeyPress:
+            keycode = event.detail
             pressKey(citrixWindow, keycode)
-    elif event.type == X.KeyRelease:
-        keycode = event.detail
-        if keycode in KEYS:
+            print('Sending key press')
+        elif event.type == X.KeyRelease:
+            keycode = event.detail
             releaseKey(citrixWindow, keycode)
 
-def processCreateEvent(event):
-    if event.type == X.PropertyNotify:
-        if event.atom == 421:
-            checkWindow(event.window)
+#def processCreateEvent(event):
+#    if event.type == X.PropertyNotify:
+#        if event.atom == 421:
+#            checkWindow(event.window)
 
-def processFocusEvent(event):
-    print 'processFocusEvent'
-    if event.type == X.FocusIn:
-        print 'Focus In'
-        if event.window == citrixWindow:
-            grabKeys(citrixWindow)
-    elif event.type == X.FocusOut:
-        print 'Focus Out'
-        if event.window == citrixWindow:
-            ungrabKeys()
+def processPropertyEvent(event):
+    print ("processPropertyEvent")
+    if event.type == X.PropertyNotify:
+        if event.atom == NET_ACTIVE_WINDOW:
+          focusedWindow = display.get_input_focus().focus
+          printWinInfo(focusedWindow)
+          checkWindow(focusedWindow)
 
 def main():
     init()
-    checkWindow(root)
+#    checkWindow(root)
     try:
         processEvents()
     except KeyboardInterrupt:
